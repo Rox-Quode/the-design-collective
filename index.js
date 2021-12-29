aa15de11bc110d10812a695801b716ebc5b5d3d9
Rox-Quode <92692755+Rox-Quode@users.noreply.github.com>
2021-12-29 23:51:03 +0200

    Added AOS

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ $('.hamburger-button').click(function(){
     $(this).toggleClass('active');
     
    });
+
+// Animate on scroll library
+AOS.init({
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false // whether animation should happen only once - while scrolling down
+   });
